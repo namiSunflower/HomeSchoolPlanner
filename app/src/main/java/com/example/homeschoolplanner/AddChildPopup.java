e52894e6199db0140b6a2bceb5e6f34225389868
namisunflower <namisunflower@byui.edu>
2021-06-24 23:27:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/homeschoolplanner/AddChildPopup.java
@@ -0,0 +1,13 @@
+package com.example.homeschoolplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddChildPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_child_popup);
+    }
+}
