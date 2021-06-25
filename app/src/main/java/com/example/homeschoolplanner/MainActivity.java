979ad371f26ef40613a06036419c9f6af867eda6
swartsd <swa17001@byui.edu>
2021-06-25 16:13:19 -0600

    Connected the firebase

--- app/src/main/java/com/example/homeschoolplanner/MainActivity.java
+++ app/src/main/java/com/example/homeschoolplanner/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.homeschoolplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "Firebase is connected", Toast.LENGTH_LONG).show();
     }
 }
