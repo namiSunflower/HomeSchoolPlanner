f8d223dd3e7977f53be74a2557071d0ae06547c5
namisunflower <namisunflower@byui.edu>
2021-06-26 12:34:01 +0800

    Added functionality for popup screen

--- app/src/main/java/com/example/homeschoolplanner/AddChild.java
+++ app/src/main/java/com/example/homeschoolplanner/AddChild.java
@@ -2,13 +2,33 @@ package com.example.homeschoolplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AddChild extends AppCompatActivity {
-
+Dialog dialog;
+ImageView exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_child);
+    }
+    public void popupSubject(View view){
+        final Dialog dialog=new Dialog(this);
+        dialog.setContentView(R.layout.activity_add_child_popup);
+        exit=(ImageView) dialog.findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 }
