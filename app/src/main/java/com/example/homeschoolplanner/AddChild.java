10f68dd4ff44fc83417f1318e95b77b12c60a54b
namisunflower <namisunflower@byui.edu>
2021-07-01 23:42:50 +0800

    Made changes to the SignUpScreen

--- app/src/main/java/com/example/homeschoolplanner/AddChild.java
+++ app/src/main/java/com/example/homeschoolplanner/AddChild.java
@@ -1,5 +1,6 @@
 package com.example.homeschoolplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
@@ -7,20 +8,44 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.NotNull;
+
+
 public class AddChild extends AppCompatActivity {
     Dialog dialog;
     ImageView exit;
+    private User child;
+    private EditText childUsername, childPassword;
+    private FirebaseAuth cAuth;
+    private String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_child);
+        cAuth = FirebaseAuth.getInstance();
+
+        childUsername = (EditText)findViewById(R.id.childUsername);
+        childPassword = (EditText)findViewById(R.id.childPassword);
+
+
     }
+    public void newChild(View v) {
+        setUpChildAccount();
+    }
+
     public void popupSubject(View v){
         final Dialog dialog=new Dialog(this);
         dialog.setContentView(R.layout.activity_add_child_popup);
@@ -38,8 +63,75 @@ public class AddChild extends AppCompatActivity {
         Intent intent = new Intent(this, SubjectsList.class);
         startActivity(intent);
     }
-    public void newChild(View v){
-        Intent intent = new Intent(this, ParentDashboard.class);
-        startActivity(intent);
+    /**
+     public void newChild(View v){
+     setUpChildAccount();
+     //Intent intent = new Intent(this, ParentDashboard.class);
+     //startActivity(intent);
+     }
+     */
+
+    public void setUpChildAccount(){
+        username = childUsername.getText().toString().trim();
+        password = childPassword.getText().toString().trim();
+        if(username.isEmpty()){
+            childUsername.setError("Username is required!");
+            childUsername.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(username).matches()){
+            childUsername.setError("Username must be a Valid email!");
+            childUsername.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            childPassword.setError("Password is required!");
+            childPassword.requestFocus();
+            return;
+        }
+        if(password.length()<6) {
+            childPassword.setError("Min Password Length is should be 6 Characters!");
+            childPassword.requestFocus();
+            return;
+        }
+        cAuth.createUserWithEmailAndPassword(username,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User child = new User(null, false,password,username,null);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(child).addOnCompleteListener(new OnCompleteListener<Void>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(AddChild.this,"User Has been Registered Successfully!",Toast.LENGTH_LONG).show();
+                                    }else {
+                                        Toast.makeText(AddChild.this,"Failed to Register User!",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(child).addOnCompleteListener(new OnCompleteListener<Void>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(AddChild.this,"User Has been Registered Successfully!",Toast.LENGTH_LONG).show();
+                                    }else {
+                                        Toast.makeText(AddChild.this,"Failed to Register User!",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+
+                    }
+                });
     }
 }
+
--- app/src/main/java/com/example/homeschoolplanner/SignUpScreen.java
+++ app/src/main/java/com/example/homeschoolplanner/SignUpScreen.java
@@ -14,90 +14,49 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.annotations.NotNull;
 
 public class SignUpScreen extends AppCompatActivity implements View.OnClickListener {
     public final static String TAG = "Signup Screen";
-    private  Button submitSignup;
+    private  Button showUser;
     private EditText editTextEmail, editTextPassword;
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private DatabaseReference reff;
+    private DatabaseReference DatbaseOfUsers;
+    private FirebaseDatabase db =FirebaseDatabase.getInstance();
+    final private String Test = "Test Directory";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
-
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-
-        submitSignup = (Button)findViewById(R.id.submitSignup);
-        submitSignup.setOnClickListener(this);
-
+        mDatabase = db.getReference();
         editTextEmail = (EditText) findViewById(R.id.emailSignup);
         editTextPassword = (EditText) findViewById(R.id.passWordSignup);
+        showUser = (Button) findViewById(R.id.showUser);
+        showUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                User user = new User(null, true, password, email, null);
+                mDatabase.child("Users").push().setValue(user);
+ }
+ });
 
-    }
+ }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.submitSignup:
-                setUpAccount();
-                break;
-        }
-    }
-
-    public void setUpAccount() {
-        String username = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-
-        if(username.isEmpty()){
-            editTextEmail.setError("Username is required!");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if(!Patterns.EMAIL_ADDRESS.matcher(username).matches()){
-            editTextEmail.setError("Username must be a Valid email!");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if(password.isEmpty()){
-            editTextPassword.setError("Password is required!");
-            editTextPassword.requestFocus();
-            return;
-        }
-        if(password.length()<6) {
-            editTextPassword.setError("Min Password Length is should be 6 Characters!");
-            editTextPassword.requestFocus();
-            return;
-        }
-        mAuth.createUserWithEmailAndPassword(username,password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            User user = new User(null, true, password, username, null);
-
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull @NotNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(SignUpScreen.this,"User Has been Registered Successfully!",Toast.LENGTH_LONG).show();
-                                    }else {
-                                        Toast.makeText(SignUpScreen.this,"Failed to Register User!",Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        }
-
-                    }
-                });
+ @Override
+ public void onClick(View v) {
+ }
+ }
 
 
 
-    }
 
-}
+
+
